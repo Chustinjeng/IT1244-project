39c4a31b466019bdbae95f709149042ea9ea4661
Chustinjeng <justcheng2000@gmail.com>
2023-10-30 19:09:14 +0800

    Insert trigram feature

--- sentiment.py
+++ sentiment.py
@@ -1,7 +1,3 @@
-import os
-import re
-import sys
-import string
 import argparse
 import datetime
 
@@ -17,20 +13,21 @@ torch.cuda.manual_seed(0)
 
 class SentDataset(Dataset):
     """
-    Define a pytorch dataset class that accepts a text path, and optionally label path and
-    a vocabulary (depends on your implementation). This class holds all the data and implement
+    A pytorch dataset class that accepts a text path, and optionally label path (only during training phase) and
+    a vocabulary (only during testing phase). This class holds all the data and implement
     a __getitem__ method to be used by a Python generator object or other classes that need it.
 
-    DO NOT shuffle the dataset here, and DO NOT pad the tensor here.
     """
-    def __init__(self, train_path, label_path=None, vocab=None):
+    def __init__(self, train_path, type, label_path=None, vocab=None):
         """
         Read the content of vocab and text_file
         Args:
             vocab (string): Path to the vocabulary file.
             text_file (string): Path to the text file.
+            type (string): Specify if model is trained using bigrams or trigrams
         """
         self.label_path = label_path
+        self.type = type
         self.texts = []
         self.labels = []
         with open(train_path, encoding='utf-8') as f:
@@ -42,24 +39,49 @@ class SentDataset(Dataset):
             self.vocabulary = {}
             curr_idx = 0
             for text in self.texts:
-                bigrams = self.generate_bigrams(text)
-                for bigram in bigrams:
-                    if bigram in self.vocabulary:
+                ngrams = self.generate_bigrams_or_trigrams(text, type)
+                for ngram in ngrams:
+                    if ngram in self.vocabulary:
                         continue
                     else:
-                        self.vocabulary[bigram] = curr_idx
+                        self.vocabulary[ngram] = curr_idx
                         curr_idx += 1
         else: 
             self.vocabulary = vocab
         
 
     def generate_bigrams(self, text):
+        """
+        Function to generate bigrams from a text (string)
+        Bigrams are defined as a grouping of a text into a list of 2 consecutive words
+        """
         tokens = text.split()
         bigrams = []
         for i in range(len(tokens) - 1):
             bigram = f"{tokens[i]} {tokens[i + 1]}"
             bigrams.append(bigram)
         return bigrams
+    
+    def generate_trigrams(self, text):
+        """
+        Function to generate bigrams from a text (string)
+        Bigrams are defined as a grouping of a text into a list of 3 consecutive words
+        """
+        tokens = text.split()
+        trigrams = []
+        for i in range(len(tokens) - 2):
+            trigram = f"{tokens[i]} {tokens[i + 1]} {tokens[i + 2]}"
+            trigrams.append(trigram)
+        return trigrams
+    
+    def generate_bigrams_or_trigrams(self, text, type):
+        """
+        Function to determine if bigrams or trigrams should be generated, depending on type specified
+        """
+        if type == "bigram":
+            return self.generate_bigrams(text)
+        else: 
+            return self.generate_trigrams(text)
 
 
     def vocab_size(self):
@@ -69,7 +91,6 @@ class SentDataset(Dataset):
         """
         return len(self.vocabulary)
 
-    
     
     def __len__(self):
         """
@@ -81,33 +102,29 @@ class SentDataset(Dataset):
         """
         Return the i-th instance in the format of:
             (text, label)
-        Text and label should be encoded according to the vocab (word_id).
+        Text and label is encoded according to the vocab (word_id).
 
-        DO NOT pad the tensor here, do it at the collator function.
         """
-        if self.label_path:
+        if self.label_path: # training
             text = self.texts[i]
             label = int(self.labels[i])
             indices = []
-            bigrams_in_text = self.generate_bigrams(text)
-            for bigram in bigrams_in_text:
-                index = self.vocabulary.get(bigram)
+            ngrams_in_text = self.generate_bigrams_or_trigrams(text, self.type)
+            for ngram in ngrams_in_text:
+                index = self.vocabulary.get(ngram)
                 indices.append(index)
     
             indices_tensor = torch.tensor(indices)
             return indices_tensor, label
         
-        else:
+        else: # testing 
             text = self.texts[i]
             indices = []
-            bigrams_in_text = self.generate_bigrams(text)
-            # print(bigrams_in_text)
-            # print("vocab: ", self.vocabulary)
-            for bigram in bigrams_in_text:
-                if bigram in self.vocabulary:
-                    index = self.vocabulary.get(bigram)
+            ngrams_in_text = self.generate_bigrams_or_trigrams(text, self.type)
+            for ngram in ngrams_in_text:
+                if ngram in self.vocabulary:
+                    index = self.vocabulary.get(ngram)
                     indices.append(index)
-            # print("indices:", indices)
             indices_tensor = torch.tensor(indices)
             return indices_tensor
 
@@ -119,21 +136,18 @@ class Model(nn.Module):
     def __init__(self, num_vocab):
         super().__init__()
         # define your model attributes here
-        # print("num_vocab: ", num_vocab)
-        self.embedding_dim = 8
-        self.embedding = nn.Embedding(num_vocab, self.embedding_dim)
-        self.first_layer_dim = 24
-        self.linear_layer_1 = nn.Linear(self.embedding_dim, self.first_layer_dim)
-        self.relu = nn.ReLU()
-        self.dropout = nn.Dropout(0.2)
-        self.linear_layer_2 = nn.Linear(self.first_layer_dim, 1)
-        self.sigmoid = nn.Sigmoid()
+        self.embedding_dim = 8 # define embedding dimensions (hyperparameter)
+        self.embedding = nn.Embedding(num_vocab, self.embedding_dim) # transform words into embeddings
+        self.first_layer_dim = 24 # define first layer dimension (hyperparameter)
+        self.linear_layer_1 = nn.Linear(self.embedding_dim, self.first_layer_dim) # linear layer
+        self.relu = nn.ReLU() # ReLU activation function
+        self.dropout = nn.Dropout(0.2) # dropout of 0.2 probability (hyperparameter) to reduce overfitting
+        self.linear_layer_2 = nn.Linear(self.first_layer_dim, 1) # last linear layer
+        self.sigmoid = nn.Sigmoid() # Sigmoid function to determine probabilities
 
 
     def forward(self, x):
-        # define the forward function here
         x = self.embedding(x)
-        # print('embedding m: ', x)
         x = torch.mean(x, dim=1)
         x = self.linear_layer_1(x)
         x = self.relu(x)
@@ -146,7 +160,7 @@ class Model(nn.Module):
 
 def collator(batch):
     """
-    Define a function that receives a list of (text, label) pair
+    A function that receives a list of (text, label) pair
     and return a pair of tensors:
         texts: a tensor that combines all the text in the mini-batch, pad with 0
         labels: a tensor that combines all the labels in the mini-batch
@@ -158,8 +172,6 @@ def collator(batch):
         labels_tensor = torch.tensor(labels, dtype=torch.float32)
         return texts_tensor, labels_tensor
     else:
-        # texts = zip(*batch)
-        # print('texts: ', list(texts))
         texts_tensor = nn.utils.rnn.pad_sequence(batch, batch_first=True, padding_value=0)
         return texts_tensor
 
@@ -169,17 +181,17 @@ def train(model, dataset, batch_size, learning_rate, num_epoch, device='cpu', mo
     Complete the training procedure below by specifying the loss function
     and optimizers with the specified learning rate and specified number of epoch.
     
-    Do not calculate the loss from padding.
     """
+    # instantiate the data loader which loads data in batches
     data_loader = DataLoader(dataset, batch_size=batch_size, collate_fn=collator, shuffle=True)
 
-    # assign these variables
+    # loss function is Binary Cross Entropy Loss
     criterion = nn.BCELoss()
+    # optimiser is Adam's optimiser
     optimizer = optim.Adam(model.parameters(), lr=learning_rate)
 
     start = datetime.datetime.now()
     for epoch in range(num_epoch):
-        # print("epoch: ", epoch)
         model.train()
         running_loss = 0.0
         for step, data in enumerate(data_loader, 0):
@@ -196,7 +208,7 @@ def train(model, dataset, batch_size, learning_rate, num_epoch, device='cpu', mo
             # calculate the loss
             loss = criterion(outputs, labels.reshape((outputs.shape[0], 1)))
 
-            # do backward propagation
+            # do backward propagation to update the weights
             loss.backward()
 
             # do the parameter optimization
@@ -213,8 +225,8 @@ def train(model, dataset, batch_size, learning_rate, num_epoch, device='cpu', mo
 
     end = datetime.datetime.now()
     
-    # define the checkpoint and save it to the model path
-    # tip: the checkpoint can contain more than just the model
+    # make the checkpoint of the model and save it to the model path
+    # contains current state of the model, optimiser, number of epochs, and current vocabulary
     checkpoint = {
         'model_state': model.state_dict(),
         'optimizer_state': optimizer.state_dict(),
@@ -233,12 +245,10 @@ def test(model, dataset, thres=0.5, device='cpu'):
     labels = []
     with torch.no_grad():
         for data in data_loader:
-            # print("data: ", data)
             texts = data.to(device)
             results = model(texts)
             pred_labels = (results > thres).int().tolist()
             pred_labels = sum(pred_labels, [])
-            # print('pred labels: ', pred_labels)
             labels.extend(pred_labels)
 
     return [str(x) for x in labels]
@@ -251,19 +261,34 @@ def main(args):
         device_str = 'cpu'
     device = torch.device(device_str)
     
+    assert args.bigram or args.trigram, "Please specify --bigram or --trigram"
+    assert not (args.bigram and args.trigram), "Please specify only --trigram or --bigram"
     assert args.train or args.test, "Please specify --train or --test"
     if args.train:
         assert args.label_path is not None, "Please provide the labels for training using --label_path argument"
-        dataset = SentDataset(args.text_path, args.label_path)
-        num_vocab = dataset.vocab_size()
-        model = Model(num_vocab).to(device)
-        
-        # specify these hyper-parameters
-        batch_size = 48
-        learning_rate = 0.01
-        num_epochs = 10
+        if args.bigram:
+            dataset = SentDataset(args.text_path, "bigram", args.label_path)
+            num_vocab = dataset.vocab_size()
+            model = Model(num_vocab).to(device)
+            
+            # specify hyper-parameters
+            batch_size = 48
+            learning_rate = 0.01
+            num_epochs = 10
 
-        train(model, dataset, batch_size, learning_rate, num_epochs, device, args.model_path)
+            train(model, dataset, batch_size, learning_rate, num_epochs, device, args.model_path)
+        else:
+            dataset = SentDataset(args.text_path, "trigram", args.label_path)
+            num_vocab = dataset.vocab_size()
+            model = Model(num_vocab).to(device)
+            
+            # specify hyper-parameters
+            batch_size = 48
+            learning_rate = 0.01
+            num_epochs = 10
+
+            train(model, dataset, batch_size, learning_rate, num_epochs, device, args.model_path)
+
     if args.test:
         assert args.model_path is not None, "Please provide the model to test using --model_path argument"
         
@@ -271,8 +296,12 @@ def main(args):
         checkpoint = torch.load(args.model_path)
 
         # create the test dataset object using SentDataset class
-        dataset = SentDataset(args.text_path, vocab=checkpoint["vocab"])
-
+        dataset = None
+        if args.bigram:
+            dataset = SentDataset(args.text_path, "bigram" ,vocab=checkpoint["vocab"])
+        else: 
+            dataset = SentDataset(args.text_path, "trigram" ,vocab=checkpoint["vocab"])
+            
         # initialize and load the model
         num_vocab = dataset.vocab_size()
         model = Model(num_vocab).to(device)
@@ -280,7 +309,6 @@ def main(args):
 
         # run the prediction
         preds = test(model, dataset, 0.5, device)
-        # print('predicted value: ', preds)
         # write the output
         with open(args.output_path, 'w', encoding='utf-8') as f:
             f.write('\n'.join(preds))
@@ -295,6 +323,8 @@ def get_arguments():
     parser.add_argument('--test', default=False, action='store_true', help='test the model')
     parser.add_argument('--model_path', required=True, help='path to the model file during testing')
     parser.add_argument('--output_path', default='out.txt', help='path to the output file during testing')
+    parser.add_argument('--bigram', action='store_true', help='train model using bigrams')
+    parser.add_argument('--trigram', action='store_true', help='train model using trigrams')
     return parser.parse_args()
 
 if __name__ == "__main__":
